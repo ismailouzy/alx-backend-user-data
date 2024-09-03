1ff4edb6d4cb762ad74d19e0112c99ab74e528ad
ismailouzy <ismailouzal@gmail.com>
2024-09-03 14:20:10 +0000

    basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+BasicAuth class for the API
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class to manage API basic authentication
+    """
+    pass
