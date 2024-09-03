1965feb4330e34da1cfa5c17c2446cf882a37f20
ismailouzy <ismailouzal@gmail.com>
2024-09-03 16:20:52 +0000

    extracting the credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -43,3 +43,19 @@ class BasicAuth(Auth):
             return decoded_str
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str,
+            ) -> str:
+        """
+        extracts the email and password from base64
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        elif type(decoded_base64_authorization_header) != str:
+            return None, None
+        elif ":" not in decoded_base64_authorization_header:
+            return None, None
+        credentials = decoded_base64_authorization_header.split(":")
+        return credentials[0], credentials[1]
