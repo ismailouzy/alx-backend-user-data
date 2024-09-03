e4871c154c130197ee0932bfceb3b4198fd530c9
ismailouzy <ismailouzal@gmail.com>
2024-09-03 16:49:23 +0000

    isntance of credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,9 @@
 """
 BasicAuth class for the API
 """
+from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
 
 
@@ -59,3 +61,28 @@ class BasicAuth(Auth):
             return None, None
         credentials = decoded_base64_authorization_header.split(":")
         return credentials[0], credentials[1]
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str,
+            ) -> TypeVar('User'):
+        """
+        returns the user instance based on the email and password
+        """
+        if user_email is None or type(user_email) != str:
+            return None
+        elif user_pwd is None or type(user_pwd) != str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+
+        if not users:
+            return None
+
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
