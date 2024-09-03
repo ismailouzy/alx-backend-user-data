14fa17c4200ef4eff615a51ae54fd731c2c9ab5e
ismailouzy <ismailouzal@gmail.com>
2024-09-03 12:51:04 +0000

    auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,7 +15,19 @@ class Auth:
         """
         Determines if authentication is required for a given path
         """
-        return False
+        if path is None:
+            return True
+        
+        if excluded_paths is None or not excluded_paths:
+            return True
+        
+        path = path.rstrip('/') + '/'
+        
+        for excluded_path in excluded_paths:
+            if path.startswith(excluded_path):
+                return False
+        
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
