3309bc2be5b439ee5611709ab2f099fbc3b5f8a1
ismailouzy <ismailouzal@gmail.com>
2024-09-03 13:12:12 +0000

    fixed pycode in auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,16 +17,16 @@ class Auth:
         """
         if path is None:
             return True
-        
+
         if excluded_paths is None or not excluded_paths:
             return True
-        
+
         path = path.rstrip('/') + '/'
-        
+
         for excluded_path in excluded_paths:
             if path.startswith(excluded_path):
                 return False
-        
+
         return True
 
     def authorization_header(self, request=None) -> str:
