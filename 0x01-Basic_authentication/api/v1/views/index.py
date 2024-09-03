a225a99c348d2592dafc682a38565555b66af249
ismailouzy <ismailouzal@gmail.com>
2024-09-03 12:40:56 +0000

    added forbidden 403

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,6 +24,16 @@ def unauthorized() -> None:
     abort(401)
 
 
+@app_views.route('/forbidden', methods=['GET']n strict_slashes=False)
+def forbidden() -> None:
+    """
+    GET /api/v1/forbidden
+    Return:
+        - Aborts with a 403 error
+    """
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
