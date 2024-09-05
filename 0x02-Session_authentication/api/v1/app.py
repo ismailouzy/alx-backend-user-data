fd054c690feef298baad028a8429bc3ea392006a
ismailouzy <ismailouzal@gmail.com>
2024-09-05 13:48:35 +0000

    created sessionauth class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -18,6 +18,9 @@ auth_type = getenv("AUTH_TYPE")
 if auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.vi.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 elif auth_type:
     from api.v1.auth.auth import Auth
     auth = Auth()
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""session auth
+"""
+
+class SessionAuth(Auth):
+    pass
