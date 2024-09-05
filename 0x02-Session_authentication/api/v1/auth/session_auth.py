acb09bf6eecd36fdc7d3689dcbfd4b5e0f09d1d8
ismailouzy <ismailouzal@gmail.com>
2024-09-05 14:33:26 +0000

    method to create id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,9 +3,22 @@
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """session auth class
     """
-    pass
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id:str = None) -> str:
+        """a method that creates a session id
+        """
+        if user_id is None:
+            return None
+        if type(user_id) != str:
+            return None
+        id = str(uuid.uuid4())
+        self.user_id_by_session_id[id] = user_id
+        return id
+
