525a6b58147a25e67600615c71d8c5409861b291
ismailouzy <ismailouzal@gmail.com>
2024-09-11 12:05:40 +0000

    user

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+User model module
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from typing import Optional
+
+Base = declarative_base()
+
+class User(Base):
+    """
+    User model class
+    """
+    __tablename__ = 'users'
+    
+    id: int = Column(Integer, primary_key=True)
+    email: str = Column(String(250), nullable=False)
+    hashed_password: str = Column(String(250), nullable=False)
+    session_id: Optional[str] = Column(String(250), nullable=True)
+    reset_token: Optional[str] = Column(String(250), nullable=True)
