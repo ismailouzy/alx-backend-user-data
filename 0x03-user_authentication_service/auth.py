25b5513114b74bc55488507c537ad30e581f4967
ismailouzy <ismailouzal@gmail.com>
2024-09-12 19:32:32 +0000

    seession id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,3 +53,13 @@ class Auth:
             return False
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """Creates a new session for the user with the given email."""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
