10989225d14da15ebb546cb9a578db19c2fa4f88
ismailouzy <ismailouzal@gmail.com>
2024-09-12 21:02:51 +0000

    done

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -106,9 +106,9 @@ def update_password():
 
     try:
         AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
